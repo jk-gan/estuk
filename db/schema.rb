9bee4b058fd40ea21120d97a0553505ddbc42d3b
Jun Kai <kuhn96@gmail.com>
2016-02-19 17:36:31 +0800

    Added the books scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218094723) do
+ActiveRecord::Schema.define(version: 20160219091100) do
+
+  create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "author"
+    t.text     "description",  limit: 65535
+    t.integer  "price"
+    t.boolean  "availability",               default: true
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
